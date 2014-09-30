3717d8d85a1f82afa91c806ce3a923e710797bb1
Dean Wampler <dean@concurrentthought.com>
2014-09-30 09:07:43 +0200

    Updated to Spark 1.1.0.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object BuildSettings {
 
   val Name = "activator-spark"
-  val Version = "2.0.0"
+  val Version = "2.1.0"
   val ScalaVersion = "2.10.4"
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq (
@@ -27,23 +27,24 @@ object Resolvers {
 
 }
 
+// We don't actually use all these dependencies, but they are shown for the
+// examples that explicitly use Hadoop.
 object Dependency {
   object Version {
-    val Spark        = "1.0.1"
-    val HadoopClient = "2.4.0"
+    val Spark        = "1.1.0"
+    val HadoopClient = "1.0.4" // "2.4.0"
     val ScalaTest    = "2.1.4"
     val ScalaCheck   = "1.11.3"
   }
 
   val sparkCore      = "org.apache.spark"  %% "spark-core"      % Version.Spark
-  val sparkStreaming = "org.apache.spark"  %% "spark-streaming" % Version.Spark   
-  val sparkSQL       = "org.apache.spark"  %% "spark-sql"       % Version.Spark   
-  val sparkHive      = "org.apache.spark"  %% "spark-hive"      % Version.Spark   
-  val sparkRepl      = "org.apache.spark"  %% "spark-repl"      % Version.Spark   
+  val sparkStreaming = "org.apache.spark"  %% "spark-streaming" % Version.Spark
+  val sparkSQL       = "org.apache.spark"  %% "spark-sql"       % Version.Spark
+  val sparkRepl      = "org.apache.spark"  %% "spark-repl"      % Version.Spark
   // Hack: explicitly add this dependency to workaround an Avro related bug
   // SPARK-1121? Appears to work! Otherwise, a java.lang.IncompatibleClassChangeError
   // is thrown in the call to saveAsParquetFile in SparkSQL9.scala.
-  val hadoopClient   = "org.apache.hadoop"  % "hadoop-client"   % Version.HadoopClient 
+  val hadoopClient   = "org.apache.hadoop"  % "hadoop-client"   % Version.HadoopClient
 
   val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.ScalaTest  % "test"
   val scalaCheck     = "org.scalacheck"    %% "scalacheck"      % Version.ScalaCheck % "test"
@@ -52,8 +53,8 @@ object Dependency {
 object Dependencies {
   import Dependency._
 
-  val activatorspark = 
-    Seq(sparkCore, sparkStreaming, sparkSQL, sparkHive, sparkRepl, hadoopClient,
+  val activatorspark =
+    Seq(sparkCore, sparkStreaming, sparkSQL, // sparkRepl, hadoopClient,
       scalaTest, scalaCheck)
 }
 
@@ -66,7 +67,7 @@ object ActivatorSparkBuild extends Build {
     id = "Activator-Spark",
     base = file("."),
     settings = buildSettings ++ Seq(
-      // runScriptSetting, 
+      // runScriptSetting,
       resolvers := allResolvers,
       libraryDependencies ++= Dependencies.activatorspark,
       unmanagedResourceDirectories in Compile += baseDirectory.value / "conf",
