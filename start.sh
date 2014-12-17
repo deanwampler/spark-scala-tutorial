74d5f1ed6dacc984dc6c1cf7a85472a9dfac2fc1
Dean Wampler <dean@concurrentthought.com>
2014-12-17 16:54:34 -0600

    Merged in changes from the Horton branch.

--- start.sh
+++ start.sh
@@ -5,84 +5,126 @@ dir=$(dirname $0)
 
 help() {
   cat <<EOF
-  usage: $0 [-h | --help] [act | activator | sbt ] [ui | shell] [options]
+  usage: $0 [-h | --help] [act | activator | sbt ] [ui | shell] \
+            [--mem N] [-nq | --not-quiet]  [options]
+
   where:
-    -h | --help       Print help and exit.
-    act | activator   Use Activator (the default).
-    sbt               Use SBT (command line interface).
-    ui | shell        (Activator only) Use the Web UI (default) or the command line shell.
-    options           Any additional options to pass to Activator or SBT.
+    -h | --help        Print help and exit.
+    act | activator    Use Activator (the default).
+    sbt                Use SBT (command line interface).
+    ui | shell         (Activator only) Use the Web UI (default) or the command line shell.
+    --mem N            The default memory for Activator is 4096 (MB), which is also used
+                       to run the non-Hadoop Spark examples. Use a larger integer value
+                       N if you experience out of memory errors.
+    -nq|--not-quiet
+                       By default, most Activator output is suppressed when the web UI is
+                       used. For debugging purposes when running the web UI, use this
+                       option to show this output.
+    options            Any additional options to pass to Activator or SBT.
 
   The default is Activator and the web-based UI.
 EOF
 }
 
-tool=activator
+filter_garbage() {
+  quiet=$1
+  while read line
+  do
+    if [[ ${line} =~ Please.point.your.browser.at ]] ; then
+      echo
+      echo "=================================================================="
+      echo
+      echo "    Open your web browser to:   http://$ip:9999"
+      echo
+      echo "=================================================================="
+    elif [[ ${line} =~ play.-.Application.started ]] ; then
+      echo $line
+    elif [[ -z $quiet ]] ; then
+      echo $line
+    fi
+  done
+}
+
+java_opts() {
+  mem=$1
+  perm=$(( $mem / 4 ))
+  (( $perm < 512 )) && perm=512
+  echo "-Xms${mem}M -Xmx${mem}M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=${perm}M"
+}
+
+mem=4096
+quiet=yes
 mode=ui
+tool=activator
 while [ $# -gt 0 ]
 do
   case $1 in
-    -h|--h*)
-      help
-      exit 0
-      ;;
-    act*)     tool=activator    ;;
-    sbt*)     tool=sbt; mode=   ;;
-    ui|shell) mode=$1           ;;
-    *)        break             ;;
+    -h|--h*)         help; exit 0      ;;
+    --mem)           shift; mem=$1     ;;
+    -nq|--not-quiet) quiet=     ;;
+    act*)            tool=activator    ;;
+    sbt*)            tool=sbt; mode=   ;;
+    ui|shell)        mode=$1           ;;
+    *)               break             ;;
   esac
   shift
 done
 
-if [[ $tool = sbt ]]
+if [[ $tool = sbt && $mode = ui ]]
 then
-  if [[ $mode = ui ]]
-  then
-    echo "$0: Can't specify 'sbt' and 'ui' together."
-    help
-    exit 1
-  elif [[ $mode = shell ]]
-  then
-    echo "$0: 'shell' argument ignored for sbt"
-    mode=
-  fi
+  echo "$0: Can't specify 'sbt' and 'ui' together."
+  help
+  exit 1
 fi
 
 dir=$(dirname $0)
 ip=$($dir/scripts/getip.sh)
 
-export act=
+act=
+opts=()
+msg=
 if [[ $tool = activator ]]
 then
   act=$(find_activator "$HOME/activator/activator")
-  [[ -z $act ]] && exit 1
   # Use http.address=0.0.0.0, because when running on a remote server,
   # The Activator UI won't listen for remote connections otherwise.
   # Invoke with NOOP=x start.sh to suppress execution:
-  opts="-Dhttp.address=0.0.0.0 -Dhttp.port=9999"
+  opts=(-Dhttp.address=0.0.0.0 -Dhttp.port=9999 $mode)
+  umode=$(echo $mode | tr '[a-z]' '[A-Z]')
+  msg="Activator, using mode $umode"
 else
   act=$(find_sbt)
-  opts=
-  [[ -z $act ]] && exit 1
+  msg="SBT"
+fi
+
+if [[ -z $act ]]
+then
+  echo "ERROR: Could not find $tool"
+  exit 1
 fi
 
 if [[ $mode = ui ]]
 then
-  umode=$(echo $mode | tr '[a-z]' '[A-Z]')
+  cat <<EOF
+==================================================================
 
-  echo "=================================================================="
-  echo
-  echo "    Starting the Spark Workshop in Activator using $umode mode..."
-  echo "    Open your web browser to:"
-  echo
-  echo "        http://$ip:9999"
-  echo
-  echo "    (Ignore a message that says 'open http://0.0.0.0:9999')"
-  echo
-  echo "=================================================================="
+    Starting the Spark Workshop in $msg
 
+==================================================================
+EOF
   sleep 2
 fi
 
-echo running: $act $opts $mode $@
-[[ -z $NOOP ]] && $act $opts $mode "$@"
+log="$dir/$tool.log"
+JAVA_OPTS=$(java_opts $mem)
+
+if [[ $mode = ui ]]
+then
+  echo "Running the Web UI. Writing all activity to $log"
+  echo running: JAVA_OPTS=\"$JAVA_OPTS\" "$act" "${opts[@]}"
+  [[ -z $NOOP ]] && ( JAVA_OPTS="$JAVA_OPTS" "$act" "${opts[@]}" 2>&1 | tee "$log" | filter_garbage $quiet )
+else
+  echo "Running the command shell. Writing all activity to $log"
+  echo running: JAVA_OPTS=\"$JAVA_OPTS\" "$act" "${opts[@]}"
+  [[ -z $NOOP ]] && ( JAVA_OPTS="$JAVA_OPTS" "$act" "${opts[@]}" 2>&1 | tee "$log" )
+fi
