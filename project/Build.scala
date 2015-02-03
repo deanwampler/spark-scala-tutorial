635d7cd1c8779dc52b4e26bcbdc2f15e42ffa64e
Dean Wampler <dean@deanwampler.com>
2015-02-03 14:36:26 -0600

    Bumped version.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object BuildSettings {
 
   val Name = "activator-spark"
-  val Version = "3.1.0"
+  val Version = "3.2.0"
   val ScalaVersion = "2.10.4"
 
   lazy val buildSettings = Defaults.coreDefaultSettings ++ Seq (
