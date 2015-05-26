2ff23fc26a1ab62adb147a7e676bb723e952d1ed
Dean Wampler <dean@concurrentthought.com>
2015-05-25 21:51:43 -0500

    Bug fixes and updates for Spark 1.4.0

--- src/main/scala/sparkworkshop/hadoop/SparkSQLParquet10-script.scala
+++ src/main/scala/sparkworkshop/hadoop/SparkSQLParquet10-script.scala
@@ -22,44 +22,39 @@ val parquetDir = "output/parquet"
 // Not needed with spark-shell:
 // val master = "yarn-client"
 // val sc = new SparkContext(master, "Spark SQL Parquet (10)")
+// val sqlContext = new SQLContext(sc)
+// import sqlContext.implicits._
 
-val sqlContext = new SQLContext(sc)
-import sqlContext._    // Make its methods accessible.
+import sqlContext.sql
 
 // Read the verses into a table, just as we did in SparkSQL9.scala:
 val lineRE = """^\s*([^|]+)\s*\|\s*([\d]+)\s*\|\s*([\d]+)\s*\|\s*(.*)~?\s*$""".r
-val verses = sc.textFile(inputDir) flatMap {
+val versesRDD = sc.textFile(inputDir) flatMap {
   case lineRE(book, chapter, verse, text) =>
     Seq(Verse(book, chapter.toInt, verse.toInt, text))
   case line =>
-    Console.err.println("Unexpected line: $line")
+    Console.err.println(s"Unexpected line: $line")
     Seq.empty[Verse]  // Will be eliminated by flattening.
 }
+val verses = sqlContext.createDataFrame(versesRDD)
 verses.registerTempTable("kjv_bible")
 
 // Save as a Parquet file:
 println(s"Saving 'verses' as a Parquet file to $parquetDir.")
 println("NOTE: You'll get an error if the directory already exists!")
-verses.saveAsParquetFile(parquetDir)
+verses.write.save(parquetDir)
 
 // Now read it back and use it as a table:
 println(s"Reading in the Parquet file from $parquetDir:")
-val verses2 = sqlContext.parquetFile(parquetDir)
+val verses2 = sqlContext.read.parquet(parquetDir)
 verses2.registerTempTable("verses2")
+verses2.show
 
 // Run a SQL query against the table:
 println("Using the table from Parquet File, select Jesus verses:")
-val jesusVerses = sql("SELECT * FROM verses2 WHERE text LIKE '%Jesus%';")
+val jesusVerses = sql("SELECT * FROM verses2 WHERE text LIKE '%Jesus%'")
 println("Number of Jesus Verses: "+jesusVerses.count())
-
-// Use the LINQ-inspired DSL that's provided by the class
-// org.apache.spark.sql.SchemaRDD that's used implicitly:
-println("GROUP BY using the LINQ-style query API:")
-// Seems like a bug that you have to import this explicitly:
-import org.apache.spark.sql.catalyst.expressions.Sum
-
-verses2.groupBy('book)(Sum('verse) as 'count).orderBy(
-  'count.desc).foreach(println)
+jesusVerses.show
 
 // Not needed if you're using the actual Spark Shell and our configured sbt
 // console command.
