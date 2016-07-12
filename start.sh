ecbb53ad288c413af48f82f6bc760c841c795c7a
Dean Wampler <dean@concurrentthought.com>
2016-07-12 12:51:09 -0500

    Removed Activator options

--- start.sh
+++ start.sh
@@ -5,44 +5,15 @@ dir=$(dirname $0)
 
 help() {
   cat <<EOF
-  usage: $0 [-h | --help] [act | activator | sbt ] [ui | shell] \
-            [--mem N] [-nq | --not-quiet]  [options]
+  usage: $0 [-h | --help] [--mem N] [options]
 
   where:
     -h | --help        Print help and exit.
-    act | activator    Use Activator (the default).
-    sbt                Use SBT (command line interface).
-    ui | shell         (Activator only) Use the Web UI (default) or the command line shell.
     --mem N            The default memory for Activator is 4096 (MB), which is also used
                        to run the non-Hadoop Spark examples. Use a larger integer value
                        N if you experience out of memory errors.
-    -nq|--not-quiet
-                       By default, most Activator output is suppressed when the web UI is
-                       used. For debugging purposes when running the web UI, use this
-                       option to show this output.
-    options            Any additional options to pass to Activator or SBT.
-
-  The default is Activator and the web-based UI.
+    options            Any additional options to pass to SBT.
 EOF
-}
-
-filter_garbage() {
-  quiet=$1
-  while read line
-  do
-    if [[ ${line} =~ Please.point.your.browser.at ]] ; then
-      echo
-      echo "=================================================================="
-      echo
-      echo "    Open your web browser to:   http://$ip:9999"
-      echo
-      echo "=================================================================="
-    elif [[ ${line} =~ play.-.Application.started ]] ; then
-      echo $line
-    elif [[ -z $quiet ]] ; then
-      echo $line
-    fi
-  done
 }
 
 java_opts() {
@@ -53,49 +24,21 @@ java_opts() {
 }
 
 mem=4096
-quiet=yes
-mode=ui
-tool=activator
+tool=sbt
 while [ $# -gt 0 ]
 do
   case $1 in
     -h|--h*)         help; exit 0      ;;
     --mem)           shift; mem=$1     ;;
-    -nq|--not-quiet) quiet=     ;;
-    act*)            tool=activator    ;;
-    sbt*)            tool=sbt; mode=   ;;
-    ui|shell)        mode=$1           ;;
     *)               break             ;;
   esac
   shift
 done
 
-if [[ $tool = sbt && $mode = ui ]]
-then
-  echo "$0: Can't specify 'sbt' and 'ui' together."
-  help
-  exit 1
-fi
-
 dir=$(dirname $0)
 ip=$($dir/scripts/getip.sh)
 
-act=
-opts=()
-msg=
-if [[ $tool = activator ]]
-then
-  act=$(find_activator "$HOME/activator/activator")
-  # Use http.address=0.0.0.0, because when running on a remote server,
-  # The Activator UI won't listen for remote connections otherwise.
-  # Invoke with NOOP=x start.sh to suppress execution:
-  opts=(-Dhttp.address=0.0.0.0 -Dhttp.port=9999 $mode)
-  umode=$(echo $mode | tr '[a-z]' '[A-Z]')
-  msg="Activator, using mode $umode"
-else
-  act=$(find_sbt)
-  msg="SBT"
-fi
+act=$(find_sbt)
 
 if [[ -z $act ]]
 then
@@ -103,28 +46,9 @@ then
   exit 1
 fi
 
-if [[ $mode = ui ]]
-then
-  cat <<EOF
-==================================================================
-
-    Starting the Spark Workshop in $msg
-
-==================================================================
-EOF
-  sleep 2
-fi
-
 log="$dir/$tool.log"
 JAVA_OPTS=$(java_opts $mem)
 
-if [[ $mode = ui ]]
-then
-  echo "Running the Web UI. Writing all activity to $log"
-  echo running: JAVA_OPTS=\"$JAVA_OPTS\" "$act" "${opts[@]}"
-  [[ -z $NOOP ]] && ( JAVA_OPTS="$JAVA_OPTS" "$act" "${opts[@]}" 2>&1 | tee "$log" | filter_garbage $quiet )
-else
-  echo "Running the command shell. Writing all activity to $log"
-  echo running: JAVA_OPTS=\"$JAVA_OPTS\" "$act" "${opts[@]}"
-  [[ -z $NOOP ]] && ( JAVA_OPTS="$JAVA_OPTS" "$act" "${opts[@]}" 2>&1 | tee "$log" )
-fi
+echo "Running SBT. Logging to $log"
+echo running: JAVA_OPTS=\"$JAVA_OPTS\" "$act" "${opts[@]}"
+[[ -z $NOOP ]] && ( JAVA_OPTS="$JAVA_OPTS" "$act" "${opts[@]}" 2>&1 | tee "$log" )
